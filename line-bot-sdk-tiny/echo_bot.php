fd7682b8780900e870cb97503b1eb2d1a9e98f4a
qoo23502004 <johnson18205@gmail.com>
2018-10-28 00:02:49 +0800

    Add files via upload

---
+++ line-bot-sdk-tiny/echo_bot.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+require_once('./LINEBotTiny.php');
+
+$channelAccessToken = 'FcMfzXISxEnOFeBCKxer6RzyRXvFpOUbegpHpgxk5ZhQR68/WjPGlY5CaBMCTNKx7XloNe4bSeKsj/BgvNpnw/oiKTbLOjb+NdWAkZK1tgT7lFpITohrcXji/dGQ8llxYjxV85CHU5b5yJQlmKH6VAdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'fd783ca5b7c8f6539f44496851e0c475';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text']
+                            )
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
